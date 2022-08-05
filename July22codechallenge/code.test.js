1b0ba9f4ab5aef668960a15760116bfba70e4c95
Niels Louwes <niels.louwes@and.digital>
2022-08-05 23:45:57 +0200

    Added more edge cases

--- July22codechallenge/code.test.js
+++ July22codechallenge/code.test.js
@@ -7,3 +7,11 @@ test('testing to see if it returns the correct substrings', () => {
 test('testing to see if nothing enters it return nothing', () => {
   expect(checkSubstringIncluded([], "")).toStrictEqual([]);
 });
+
+test('testing to see if it works in a single substring', () => {
+  expect(checkSubstringIncluded(["blah", "bar"], "b")).toStrictEqual(["blah", "bar"]);
+})
+
+test('testing to see if it works with words in all caps', () => {
+  expect(checkSubstringIncluded(["BLAH", "BAR"], "b")).toStrictEqual(["BLAH", "BAR"]);
+})
