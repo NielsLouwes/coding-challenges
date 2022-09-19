fdc968fb3d0eda6a0b5edce4f569e7a05b73eae4
Niels Louwes <niels.louwes@and.digital>
2022-09-19 17:10:34 +0200

    shorter solution added

--- Sept19Actual/code.js
+++ Sept19Actual/code.js
@@ -1,11 +1,15 @@
 function inviteMoreWomen(L) {
   const men = L.filter((item) => item > 0);
   const women = L.filter((item) => item < 0);
-  // if women same length or less, don't need to invite more so its false
-    if (women.length < men.length) {
+  if (women.length < men.length) {
       return true;
     }
     return false;
+  }
+
+  // shorter solution is to use reduce to add everything up and then if the sum is more than 0, you need more women (-1) to make it even. 
+  function shorterInviteMoreWomen(L) {
+    return L.reduce((a,b) =>  a + b > 0)
   }
 
   module.exports =  inviteMoreWomen;
