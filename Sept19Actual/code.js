76fd7e13002a8645ace710df7a12a92cc4ba9265
Niels Louwes <niels.louwes@and.digital>
2022-09-19 17:06:21 +0200

    refactors

--- Sept19Actual/code.js
+++ Sept19Actual/code.js
@@ -2,7 +2,7 @@ function inviteMoreWomen(L) {
   const men = L.filter((item) => item > 0);
   const women = L.filter((item) => item < 0);
   // if women same length or less, don't need to invite more so its false
-    if (women.length < men.length || women.length == men.length) {
+    if (women.length < men.length) {
       return true;
     }
     return false;
