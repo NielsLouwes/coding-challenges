49b47b9cde0abc868ad1fcddd2c1d215bfa500dd
Niels Louwes <niels.louwes@and.digital>
2022-09-28 09:51:26 +0200

    added 1 more test for negative case

--- 26Sept/codeSept26.test.js
+++ 26Sept/codeSept26.test.js
@@ -8,4 +8,8 @@ describe('sortOddsOnly function', () => {
     test('should return list with odd numbers sorted along with even numbers in original position of OG array', () => {
       expect(sortOddsOnly([3,1,2,5,6,3])).toEqual([1,3,2,3,6,5]);
     })
+
+     test('should return correct list even with negative numbers', () => {
+      expect(sortOddsOnly([3,-1,2,5,6,3])).toEqual([-1,3,2,3,6,5]);
+    })
   });
