c93ddde811bff3e0217fd99e18fa2be4937b4143
Niels Louwes <niels.louwes@and.digital>
2022-10-25 14:49:38 +0200

    working solution

--- 24Oct/code24Oct.js
+++ 24Oct/code24Oct.js
@@ -8,48 +8,23 @@
 const keyboards = [10,20,30];
 const drives = [3,5,8];
 const money = 25;
-
-// const affordKeyboards = keyboards.filter(keyboard => keyboard < money)
-// console.log('affordKeyboards', affordKeyboards);
-
-// const affordDrives = drives.filter(drive => drive < money)
-// console.log('affordDrives', affordDrives);
-// ;
-// const combineLists = [...keyboards, ...drives];
-// console.log('combinedLists', combineLists);
-
-
-
 function getMoneySpent(keyboards = [], drives = [], b = 0) {
-    let max = -1;
+    let maxPrice = -1;
      
     for(let i = 0; i < keyboards.length; i++) {
         const keyboardPrice = keyboards[i];
         for(let j = 0; j < drives.length; j++) {
             const drivePrice = drives[j];
-            if (keyboardPrice + drivePrice <= b && keyboardPrice + drivePrice > max) {
-                max = keyboardPrice + drivePrice;
+            if (keyboardPrice + drivePrice <= b && keyboardPrice + drivePrice > maxPrice) {
+                maxPrice = keyboardPrice + drivePrice;
             }
         }
     }
-    return max;
+    return maxPrice;
 }
 
 console.log('getMoneySpent', getMoneySpent([10,20,30], [3,5,8], 40));
+console.log('getMoneySpent', getMoneySpent([10,20,30], [3,5,8], 13));
 
 module.exports = getMoneySpent;
 
- 
-
-
-  //   const checkCombinedPrices = keyboards.map((keyboard) => {
-  //   let max = -1;
-  //   drives.map((drive) => {
-  //     if (keyboard + drive <= b && keyboard + drive > max) {
-  //      return checkCombinedPrices;
-  //     }
-  //   })
-  //   return max;
-  // })
-
-  // https://stackoverflow.com/questions/44131209/using-a-map-within-a-map-in-jsx
