929740f5c06e313c08fe40a80d17e33225ea6404
Niels Louwes <niels.louwes@and.digital>
2022-10-26 11:51:17 +0200

    removed comments and logs

--- 24Oct/code24Oct.js
+++ 24Oct/code24Oct.js
@@ -1,13 +1,3 @@
-/* Step by step 
-1. B is money available
-2. keyboards and drives are arrays with prices
-3. keyboard[i] + drives[i], must not exceed b.
-4. filter them out, sort those, return the largest number that can be afforded
-*/
-
-const keyboards = [10,20,30];
-const drives = [3,5,8];
-const money = 25;
 function getMoneySpent(keyboards = [], drives = [], b = 0) {
     let maxPrice = -1;
      
