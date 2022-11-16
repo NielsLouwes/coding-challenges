d578713eb4d3f786ce756e7381fa8699f849e051
Niels Louwes <niels.louwes@and.digital>
2022-11-16 10:09:05 +0100

    refactor

--- Nov14/codeNov14.js
+++ Nov14/codeNov14.js
@@ -1,19 +1,7 @@
-/* 
-I need to check if I can divide the index by with no remainder
-// filter out all elements that are located at an even index in the array.
-let x = arr.filter((element, index) => {
-  return index % 2 === 0;
-})
-I need to also checks for adds.
-Then use every to make sure each in their individual arrays match what I need.
-*/
-
 function isSpecialArray(arr = []) {
-  //return even indexs in a new array
   const evenIndexes = arr.filter((_, index) => {
     return index % 2 === 0;
   });
-  //return odd indexes in a new array
   const oddIndexes = arr.filter((_, index) => {
     return index % 2 !== 0;
   });
@@ -34,7 +22,6 @@ function isSpecialArray(arr = []) {
 
 module.exports = isSpecialArray;
 
-console.log("isSpecialArray", isSpecialArray([0, 1, 4, 3, 4, 5]));
-console.log('emptyArray', isSpecialArray([]));
+
 
 
