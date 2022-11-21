5d93a0af144b2f6c64d6c8e73197c81f35e59d22
Niels Louwes <niels.louwes@and.digital>
2022-11-21 13:45:24 +0100

    pseudo + figuring it out

--- Nov21/codeNov21.js
+++ Nov21/codeNov21.js
@@ -1,6 +1,26 @@
+
+const jewelry = {
+  "Diamond Earrings": 980,
+  "Gold Watch": 250,
+  "Pearl Necklace": 4650
+}
+
+
 function mostExpensive(obj) {
 	
 }
+
+/*
+1. Turn the values from the object into arr with object.values
+2. find max of that. You need to spreasd the array.
+3. return the sentence
+*/
+let obj = { a: 4, b: 0.5 , c: 0.35, d: 5 };
+let arr = Object.values(obj);
+console.log(arr, "arr")
+
+let max = Math.max(...arr);
+console.log('the biggest number is: ', max);
 
 module.exports = mostExpensive;
 
