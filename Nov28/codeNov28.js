8cf1d25eba53d9b23ec28a83dbc3cc44a4212871
Niels Louwes <niels.louwes@and.digital>
2022-11-30 11:28:40 +0100

    added validation for top and bottom of box

--- Nov28/codeNov28.js
+++ Nov28/codeNov28.js
@@ -7,16 +7,21 @@
 */
 
 const testArr = [
-  "###",
-  "#*#",
-  "###"
+  "###", // the first arrayItem needs to have every item being # AND minimum length of 3
+  "#*#", // the second arrayItem should have a * in not the last AND first position AND must exist
+  "###" // the last arrayItem should have minimum 3 # signs and all contain #
 ]
 
 const numbers = [1,2,3,4,5, '*']
 const asterisk = '*'
 
 const firstPart = testArr[0].split('')[0]; // split up into individual parts so I can check the 0th index of that.
-const lastPart = testArr[testArr.length - 1].split(''); // split up the final party of array
+const firstPartAllPoundSigns = testArr[0].every((item) => item === '#'); // return true if every item is a pound sign 
+const atLeastThreePoundSigns = testArr[0].length > 2 && testArr[2].length > 2; // checks for length of # signs on top and bottom border
+
+if (firstPartAllPoundSigns && atLeastThreePoundSigns === true)
+console.log('firstPartAllPoundSigns', firstPartAllPoundSigns);
+const lastPart = testArr[testArr.length - 1].split(''); // split up the final party of arrays
 
 
 console.log('lastPart', lastPart);
