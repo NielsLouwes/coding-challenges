11829581da4cefc0b255268a6bbd43a318a05bee
Niels Louwes <niels.louwes@and.digital>
2022-12-01 15:47:13 +0100

    solved first and last row correctly

--- Nov28/codeNov28.js
+++ Nov28/codeNov28.js
@@ -1,3 +1,4 @@
+
 /* 
 1. Look for the asterisk *
 2. If it doesn't exist = false
@@ -10,52 +11,60 @@ const testArr = [
   "###", // the first arrayItem needs to have every item being # AND minimum length of 3
   "#*#", // the second arrayItem should have a * in not the last AND first position AND must exist
   "###" // the last arrayItem should have minimum 3 # signs and all contain #
-]
+];
 
-const numbers = [1,2,3,4,5, '*']
-const asterisk = '*'
+const asterisk = "*";
 
-const firstPart = testArr[0].split('')[0]; // split up into individual parts so I can check the 0th index of that.
-const firstPartAllPoundSigns = testArr[0].every((item) => item === '#'); // return true if every item is a pound sign 
-const atLeastThreePoundSigns = testArr[0].length > 2 && testArr[2].length > 2; // checks for length of # signs on top and bottom border
+// PART 1
 
-if (firstPartAllPoundSigns && atLeastThreePoundSigns === true)
-console.log('firstPartAllPoundSigns', firstPartAllPoundSigns);
-const lastPart = testArr[testArr.length - 1].split(''); // split up the final party of arrays
+const firstPart = testArr[0].split(""); // split up into individual parts so I can check the 0th index of that.
+const lastPart = testArr[testArr.length - 1].split(""); // split up the final party of arrays
 
+const meetsThreePoundSignsCriteria = (currentValue) => currentValue === "#";
+const containsAtleastThreePoundSignsBeginning = firstPart.every(
+  meetsThreePoundSignsCriteria
+);
+const containsAtleastThreePoundSignsEnd = lastPart.every(
+  meetsThreePoundSignsCriteria
+);
 
-console.log('lastPart', lastPart);
-console.log('testArr[0]', testArr[0]);
-console.log('firstPart', firstPart);
+console.log(
+  "First array contains at least three # signs ###",
+  containsAtleastThreePoundSignsBeginning
+);
+console.log(
+  "last array row should contain at least three ###",
+  containsAtleastThreePoundSignsEnd
+);
+
+// if (firstPartAllPoundSigns && atLeastThreePoundSigns === true) - our functino should check for this to be true before checking on the middle bit.
+
+console.log("lastPart", lastPart);
+
+console.log("firstPart", firstPart);
 
 const checkFirst = firstPart.includes(asterisk); // I'm checking if the firstPart array has the asterisk , or includes it.
 const checkLast = lastPart.includes(asterisk); // Also checking if the last Pary final indexed item has the asterisk
-console.log('checkFirst', checkFirst);
-console.log('checkLast', checkLast);
+console.log("checkFirst", checkFirst);
+console.log("checkLast", checkLast);
 
-//First part needs to be a #
+//First part needs to be a # and there must be an astrisk inside
 
-
-const filterAsterisk = numbers.some((element) => element === "*");
-console.log('filterAsterisk', filterAsterisk);
-
-const findAsterisk = testArr.some((element) => element === '#*#');
-console.log('findAsterisk', findAsterisk);
-
+const findAsterisk = testArr.some((element) => element === "#*#");
+console.log("findAsterisk", findAsterisk);
 
 function inBox(arr) {
-	const asterisk = '*';
+  const asterisk = "*";
 
   //Capture both first item in first array item and last item in last array item
-  const firstItem = arr[0].split('')[0];
-  const lastItem = arr[arr.length - 1].split('');
+  const firstItem = arr[0].split("")[0];
+  const lastItem = arr[arr.length - 1].split("");
 
   // check if both cases includes the asterisk, if so, the function returns false because it should be in the box
   const checkFirst = firstPart.includes(asterisk);
-  const checkLast = lastPart.includes(asterisk);    
+  const checkLast = lastPart.includes(asterisk);
 
   if (checkFirst || checkLast === true) {
     return false;
   }
-} 
-
+}
