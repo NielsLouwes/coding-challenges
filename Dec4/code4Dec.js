a84be3eb4c3280484561875b997a1c115a255e28
Niels Louwes <niels.louwes@and.digital>
2022-12-08 15:36:03 +0100

    added solution

---
+++ Dec4/code4Dec.js
@@ -0,0 +1,10 @@
+const isGoodMatch = (arr = []) => {
+  if (arr.length % 2 !== 0) {
+    return "bad match";
+  }
+  let result = [];
+  for (let i = 0; i < arr.length - 1; i += 2) {
+    result.push(arr[i] + arr[i + 1]);
+  }
+  return result;
+};
