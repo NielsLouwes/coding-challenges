4f3455330731c25eec33674ff653d1241f53bce7
Niels Louwes <niels.louwes@and.digital>
2023-02-20 17:13:03 +0100

    Added solution and tests

--- 20Feb/code20Feb.js
+++ 20Feb/code20Feb.js
@@ -1,3 +1,9 @@
-function removeFalsy(num = 0, length = 0) {}
+function removeFalsy(data = []) {
+    return data.filter((item) => !!item)
+}
+
+console.log(removeFalsy([0, 1, false, 2, "", 3]))
+
 
 module.exports = removeFalsy;
+
--- 20Feb/code20Feb.test.js
+++ 20Feb/code20Feb.test.js
@@ -1,7 +1,31 @@
 const removeFalsy = require("./code20Feb");
 
 describe("removeFalsy function", () => {
-  test("should return 5 multiples of 7 when 7 and 5 are entered as arguments", () => {
-    expect(removeFalsy(7, 5)).toEqual([7, 14, 21, 28, 35]);
+  test("should return only truthy values if both types of values are provided", () => {
+    const input = [0, 1, false, 2, "", 3];
+    const result = [1,2,3]
+
+    expect(removeFalsy(input)).toEqual(result);
+  });
+
+  test("should return only an empty array if only falsey values are provided", () => {
+    const input = [0, false ,""];
+    const result = []
+
+    expect(removeFalsy(input)).toEqual(result);
+  });
+
+   test("should return empty array if no values are input", () => {
+    const input = []
+    const result = []
+
+    expect(removeFalsy(input)).toEqual(result);
+  });
+
+   test("should return only truthy values if edge cases are provided", () => {
+    const input = [false, null, 0, "", undefined, NaN, 1, "hello", true, {}, [], Infinity]
+    const result = [1, "hello", true, {}, [], Infinity]
+
+    expect(removeFalsy(input)).toEqual(result);
   });
 });
