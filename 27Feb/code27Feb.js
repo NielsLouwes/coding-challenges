f5a0aa178f4f8a47054cacb398745932f2c0fcb6
Niels Louwes <niels.louwes@and.digital>
2023-02-21 16:47:17 +0100

    simple solution 27 feb

--- 27Feb/code27Feb.js
+++ 27Feb/code27Feb.js
@@ -1,7 +1,25 @@
-const checkEnoughMoney = () => {
+const checkEnoughMoney = (change, amountDue) => {
+  const quarters = change[0] * 0.25;
+  const dimes = change[1] * 0.1;
+  const nickels = change[2] * 0.5;
+  const pennies = change[3] * changeValueArray[3];
 
-}
+  const totalChange = quarters + dimes + nickels + pennies;
 
+  return totalChange >= amountDue;
+};
+
+const checkEnoughDynamic = (change, amountDue) => {
+  const changeValueArray = [0.25, 0.1, 0.5, 0.1];
+};
+
+console.log(checkEnoughMoney([20, 0, 10, 5], 20.0));
 
 module.exports = checkEnoughMoney;
 
+/*
+1. the order of the items in array is always representing the type of change
+2. map over the change array, 
+
+
+*/
