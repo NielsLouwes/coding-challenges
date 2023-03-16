b81c2717063cb453cd784e658fa0f944abf18a1b
Niels Louwes <niels.louwes@and.digital>
2023-03-16 17:13:45 +0100

    set up 13 march challenge

---
+++ 13March/code13March.js
@@ -0,0 +1,11 @@
+const groceryStore = new Map();
+
+groceryStore.set("cheese", 4.0);
+groceryStore.set("ham", 2.0);
+groceryStore.set("milk", 1.0);
+
+const getPhoneNumberCombinations = () => {
+  
+};
+
+module.exports =  getPhoneNumberCombinations;
