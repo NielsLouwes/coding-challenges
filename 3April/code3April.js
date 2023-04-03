0e5875fe11d9ae99accb64f2c8c519f3feceabe2
Niels Louwes <niels.louwes@and.digital>
2023-04-03 14:14:26 +0200

    added tests

--- 3April/code3April.js
+++ 3April/code3April.js
@@ -1,10 +1,28 @@
-const handOfStraights = (hand, groupSize) => {
-  if (!Number.isInteger(groupSize) || groupSize === 0) {
-    return false;
+const mergeIntervals = (intervalArray = []) => {
+  const flatten = intervalArray.flat();
+
+  for (let i = 0; i < flatten.length - 1; i++) {
+    if (flatten[i] > flatten[i + 1]) {
+      flatten.splice(i, 2); // remove both elements
+      i--; // adjust index to account for removed elements
+    }
   }
-  return hand.length % groupSize === 0;
-   
+
+  const finalArray = [];
+  for (let i = 0; i < flatten.length; i += 2) {
+    finalArray.push([flatten[i], flatten[i + 1]]);
+  }
+
+  return finalArray;
 };
 
-module.exports = handOfStraights;
+console.log(
+  mergeIntervals([
+    [1, 3],
+    [2, 6],
+    [8, 10],
+    [15, 18]
+  ])
+);
 
+module.exports = mergeIntervals;
--- 3April/code3April.test.js
+++ 3April/code3April.test.js
@@ -1,15 +1,32 @@
 const mergeIntervals = require("./code3April");
 
-describe("handOfStraights function", () => {
-  it("should return true if the groupSize is divisible by the hand length", () => {
-    expect(mergeIntervals([1,2,3,4], 2)).toEqual(true);
+describe("mergeIntervals function", () => {
+  it("should return the original array of arrays if there are no overlapping intervals", () => {
+    const input = [
+      [1, 2],
+      [3, 4],
+    ];
+    const noOverLapSolution = [
+      [1, 2],
+      [3, 4],
+    ];
+
+    expect(mergeIntervals(input)).toEqual(noOverLapSolution);
   });
 
-  it("should return false if the groupSize is NOT divisible by the hand length", () => {
-    expect(mergeIntervals([1,2,3,4], 3)).toEqual(false);
+  it("should return the new array of arrays if there are overlapping intervals", () => {
+    const input = [
+      [1, 3],
+      [2, 6],
+      [8, 10],
+      [15, 18],
+    ];
+    const overlapResult = [[1,6], [8,10], [15,18]];
+    
+    expect(mergeIntervals(input)).toEqual(overlapResult)
   });
 
-  it("should return false if the groupSize is a full integer", () => {
-    expect(mergeIntervals([1,2,3,4,5], 1.5)).toEqual(false);
+  it("should return an empty array if nothing is entered", () => {
+    expect(mergeIntervals()).toEqual([]);
   });
 });
