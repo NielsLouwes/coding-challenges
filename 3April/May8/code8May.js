e2f5eede63fac1b46cc73058d4509e6a5c2d30cd
Niels Louwes <niels.louwes@and.digital>
2023-05-11 09:11:04 +0200

    cleanup

--- 3April/May8/code8May.js
+++ 3April/May8/code8May.js
@@ -15,12 +15,4 @@ const returnAnagrams = (strings) => {
   return Object.values(anagrams);
 };
 
-const strs1 = ["eat", "tea", "tan", "ate", "nat", "bat"];
-const strs2 = [""];
-const strs3 = ["a"];
-
-// console.log(returnAnagrams(strs1)); // [["eat","tea","ate"],["tan","nat"],["bat"]]
-// console.log(returnAnagrams(strs2)); // [[""]]
-// console.log(returnAnagrams(strs3)); // [["a"]]
-
 module.exports = returnAnagrams;
