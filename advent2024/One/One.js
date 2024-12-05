ed474a93000a94a11d81d95ee18b189faba19d0d
Niels Louwes <nielslouwes45@gmail.com>
2024-12-05 19:40:10 +0100

    part 2 pre-solution

--- advent2024/One/One.js
+++ advent2024/One/One.js
@@ -42,6 +42,21 @@ const readInputFile = async () => {
 
     addDifferences();
 
+    // PART TWO 1. go through arr1, get the count of curent item found in the other list. Take that count and multiply it by current item in arr1
+
+    // The first number in the left list is 3. It appears in the right list three times, so the similarity score increases by 3 * 3 = 9.
+    // The third number in the left list is 2. It does not appear in the right list, so the similarity score does not increase (2 * 0 = 0).
+
+    const calculateSimilarity = () => {
+      arr1.map((item) => {
+        let count = 0;
+
+        // arr2.map((item2) => {
+        //   if (item === item2)
+        // })
+      });
+    };
+
     console.log("result", result);
   } catch (err) {
     console.error("Error reading file:", err);
@@ -49,3 +64,33 @@ const readInputFile = async () => {
 };
 
 readInputFile();
+
+const list1 = [3, 4, 2, 1, 3, 3];
+const list2 = [4, 3, 5, 3, 9, 3];
+
+// find the # of occurences of each item in list one found in list
+
+const findOccurences = () => {
+  const dictionary = {};
+
+  list1.map((item1) => {
+    // build our dict
+    if (!dictionary[item1]) {
+      dictionary[item1] = 0;
+    }
+
+    list2.map((item2) => {
+      if (item1 === item2) {
+        dictionary[item1]++;
+      }
+    });
+  });
+
+  const counts = Object.values(dictionary).reduce((acc, curr) => {
+    return acc + curr;
+  }, 0);
+
+  return counts;
+};
+
+findOccurences();
