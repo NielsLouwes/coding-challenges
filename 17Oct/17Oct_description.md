12e4a8f668187e8abd24c91b9e441b54aabb3380
Niels Louwes <nielslouwes45@gmail.com>
2025-01-14 16:58:54 +0100

    17 oct leaderboard solution partial

--- 17Oct/17Oct_description.md
+++ 17Oct/17Oct_description.md
@@ -1,19 +1,17 @@
 # Example
+
 Leaderboard = [100,90,90,80]
 Player = [70,80,105]
 The Leaderboard will have ranks 1,2,2 and 3 respectively. If the player's scores are 70, 80, 105, their rankings after each game are 4th, 3rd, and 1st.
-Output in this case would be =  [4,3,1}.
+
+Output in this case would be = [4,3,1}.
 
 ## Important details:
+
 The existing leaderboard leaderboard[] is in descending order.
-The player's scores player[]  are in ascending order.
+The player's scores player[] are in ascending order.
 
+## To do
 
- 1. Combine the leaderboard and player arrays
-2. If leaderboard item === players item, return its index
-3. if (index === index) {
-  return rank
-}
-rank = index + 1
-if playerScore item === leaderboard item after combining, return its index.
-
+1. Put the playerscore into the leaderboard
+2. IF in top 5 , let player know their high score and position in leaderboard.
--- 17Oct/code17Oct.js
+++ 17Oct/code17Oct.js
@@ -1,14 +1,30 @@
-function climbingLeaderboard(leaderboard = [], playerScores = []) {
-  const addPlayerToLeaderboard = [...leaderboard, ...playerScores];
-  const uniqueValuesLeaderboard = [...new Set(addPlayerToLeaderboard)].sort(
-    (a, b) => b - a
-  );
-  const returnIndexPlayerScores = playerScores.map((searchTerm) =>
-    uniqueValuesLeaderboard.findIndex(
-      (compareTerm) => compareTerm === searchTerm
-    )
-  );
-  return returnIndexPlayerScores.map((score) => score + 1);
+let leaderBoard = [100, 90, 90, 80];
+
+function updateLeaderBoard(playerScore) {
+  leaderBoard.push(playerScore);
+  leaderBoard.sort((a, b) => b - a);
+
+  const scorePosition = leaderBoard.findIndex((score) => score === playerScore);
+  console.log("scorePosition", scorePosition);
+
+  if (scorePosition < 6) {
+    console.log(
+      `Congrats, you achieved the ${
+        scorePosition + 1
+      }th score of ${playerScore}!`
+    );
+  } else {
+    console.log(
+      `You scored a ${playerScore}, but this score is not in the top 5. Try again to become a legend.`
+    );
+  }
+  leaderBoard = leaderBoard.slice(0, 5);
+  return leaderBoard;
 }
 
-module.exports = climbingLeaderboard;
+console.log("updateLeaderBoard(82)", updateLeaderBoard(82));
+console.log("leaderBoard", leaderBoard);
+
+console.log("updateLeaderBoard(82)", updateLeaderBoard(81));
+console.log("updateLeaderBoard(82)", updateLeaderBoard(81));
+console.log("updateLeaderBoard(82)", updateLeaderBoard(55));
