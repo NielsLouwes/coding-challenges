a23218f0aab4a4618e6fb9bd1725d29ef6f831f3
Niels Louwes <nielslouwes45@gmail.com>
2025-02-21 10:46:17 +0100

    arr of multiples solution

--- 13Feb/code13Feb.js
+++ 13Feb/code13Feb.js
@@ -1,15 +1,34 @@
-
+// first solution - recursion
 function getArrayOfMultiples(num = 0, length = 0) {
-  const result = [];
-  for (let i = 0; i < length + 1; i++) {
-    result.push(num * i)
-    
+  // num multiplied length amount of times, but show each iteration
+
+  if (length === 0) {
+    return [];
   }
-  return result.slice(1, result.length)
+
+  let result = getArrayOfMultiples(num, length - 1); // Recursive call
+  result.push(num * length); // Push the current multiple
+  console.log("result", result);
+
+  return result; // Return the accumulated result
 }
 
-console.log(getArrayOfMultiples(7,5));
+// console.log(getArrayOfMultiples(7, 5));
 
+// 2nd solution - my way
 
+function secondGetArrOfMultiples(num = 0, length = 0) {
+  let result = [];
+  let multiple = length;
+
+  for (let i = 0; i < length; i++) {
+    result.push(num * multiple);
+    multiple--;
+  }
+
+  return result.sort((a, b) => a - b);
+}
+
+secondGetArrOfMultiples(7, 5);
 
 module.exports = getArrayOfMultiples;
