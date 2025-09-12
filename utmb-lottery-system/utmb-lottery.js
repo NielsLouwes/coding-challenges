d17991d860623b4599befe0361ceb1b5c9b7bda5
Niels Louwes <nielslouwes45@gmail.com>
2025-09-12 12:33:36 +0200

    create utmb lottery better solution

--- utmb-lottery-system/utmb-lottery.js
+++ utmb-lottery-system/utmb-lottery.js
@@ -43,24 +43,41 @@ const createLotteryList = () => {
 
    for (let i = 0; i < tokenCount; i++) {
      userList.push(person.name)
-  }
+    }
   })
 
   return userList;
 }
 
 const lotteryList = createLotteryList();
-console.log('lotteryList', lotteryList)
-
 
 const getWinner = () => {
   const winner = Math.floor(Math.random() * lotteryList.length)
-
   const result = lotteryList[winner]
-  console.log('result', `The winner is ${result}`)
+  
+  console.log(`The winner is ${result}`)
   return result;
 }
 
-getWinner()
+// getWinner()
 
-// solution 2 - better time complexity
+// solution 2 - better time complexity - weighted random selection
+
+const createWeightedDistribution = () => {
+  // step 1: get total weight of participant tokens
+  let totalWeight = participants.reduce((acc, curr) => acc + curr.tokens, 0);
+  // generate a random number in the range of 0 - total tokens
+  const generateRandomNumber = Math.floor(Math.random() * totalWeight);
+  
+  let runningSum = 0;
+ // step 3: iterate over participans, adding their token to total sum
+  for (let person of participants){
+    runningSum += person.tokens;
+    // step 4: if the randomNumber is less than runningSUM, that means we hit our winner
+    if (generateRandomNumber < runningSum) {
+        return `The winner is ${person.name}!`;
+    }
+  }
+}
+
+console.log(createWeightedDistribution())
